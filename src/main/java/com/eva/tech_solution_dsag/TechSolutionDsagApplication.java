4a7c6fdf6e6994f9efce47b174360dcc876e8994
sebas0040 <deividsebastianarmero@gmail.com>
2026-04-15 07:44:19 -0500

    first commit

---
+++ src/main/java/com/eva/tech_solution_dsag/TechSolutionDsagApplication.java
@@ -0,0 +1,13 @@
+package com.eva.tech_solution_dsag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionDsagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionDsagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eva/tech_solution_dsag/TechSolutionDsagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eva.tech_solution_dsag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSolutionDsagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
